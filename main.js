067f53e06776467def6aeb7fc1ac121c41088693
svetlana18055@yandex.ru <svetlana18055@yandex.ru>
2021-10-02 17:09:12 +0300

    задание 2го урока

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
 "use strict"
 
+let num = 266219
+let strFromNum = num.toString();
+let numArray = strFromNum.split('');
+console.log(numArray)
+
+let result = numArray.reduce(function(a, b) {
+    return a * b;
+  });
+  console.log(result)
+
+  result **= 3
+  console.log(result) 
+
+
+  let strFromResult = result.toString()
+  let reducedStrFromResult = strFromResult.substring(0, 2)
+      console.log(reducedStrFromResult)
+
+ 
+
+
