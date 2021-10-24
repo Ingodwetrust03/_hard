42605687625ff82a5ab74a9c0e79b707e1bab6d4
svetlana18055@yandex.ru <svetlana18055@yandex.ru>
2021-10-24 23:13:35 +0300

    lesson hard 15

--- main.js
+++ main.js
@@ -1,58 +1,71 @@
 'use strict'
-const showDate = {
-    dateForDateWithText: {},
-    dateForDateWithoutText: {},
-    dateWithText: {},
-    currentDay: 0,
-    currentMonth: 0,
-    block: {},
-    timerIdOne: {},
-    timerIdTwo: {},
-    rusDays: ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
-    rusMonths: ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнб", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"],
-    hours: ['час', 'часов', 'часа'],
-    hoursText: {},
-    chas: '',
-    chasa: '',
-    chasov: '',
-    arrayToRemove: [],
-    showDateWithText: function() {
-        showDate.dateForDateWithText = new Date();
-        showDate.currentDay = showDate.dateForDateWithText.getDay()
-        showDate.currentMonth = showDate.dateForDateWithText.getMonth()
-        showDate.dateWithText = document.createElement('div')
-        showDate.dateWithText.innerHTML = 'Сегодня ' + showDate.rusDays[showDate.currentDay] + ', ' + showDate.dateForDateWithText.getDate() + ' ' + showDate.rusMonths[showDate.currentMonth] + ' ' + showDate.dateForDateWithText.getFullYear() + ' года ' + showDate.dateForDateWithText.getHours() + " час " + showDate.dateForDateWithText.getMinutes() + " минут " + showDate.dateForDateWithText.getSeconds() + ' секунд '
-        showDate.dateWithText.classList.add('date-with-text')   
-        document.body.append(showDate.dateWithText)
-    },
-    showDateWithoutText: function() {
-        showDate.arrayToRemove = document.querySelectorAll('.date, .date-with-text')
-        showDate.dateForDateWithoutText = new Date();
-        showDate.currentMonthNum = showDate.dateForDateWithoutText.getMonth()
-        showDate.dateWithoutText = document.createElement('div')
-        showDate.dateWithoutText.innerHTML = showDate.dateForDateWithoutText.getDate() + "." + (showDate.currentMonthNum + 1) + '.' + showDate.dateForDateWithoutText.getFullYear() + ' - ' + showDate.dateForDateWithoutText.getHours() + ":" + showDate.dateForDateWithoutText.getMinutes() + ":" + showDate.dateForDateWithoutText.getSeconds()
-        showDate.dateWithoutText.classList.add('date')   
-        document.body.append(showDate.dateWithoutText)
-    },
-    grammar: function() {
-        hoursText = function() {
-            chas = showDate.dateForDateWithoutText.getHours()[0] + ' час'
-            chasa = function() {
-                for(let i = 1; i <= 4; i++) {
-                    return showDate.dateForDateWithoutText.getHours()[i] + ' часа'
-                }
-            }
-            chasov = function() {
-                for(let i = 4; showDate.hours.length <= 4; i++) {
-                    return showDate.dateForDateWithoutText.getHours()[i] + ' часов'
-                }
-            }
-        }
-    },
-    start: function() {
-        showDate.timerIdOne = setInterval(showDate.showDateWithText, 1000);
-        showDate.timerIdTwo = setInterval(showDate.showDateWithoutText, 1000);
-        showDate.grammar()
-    }
+
+const form = document.querySelector('.create-element')
+let newBlock
+let shiftingDiv
+let divToShift
+
+const DomElement = function(selector, height, width, bg, fontSize){
+    this.selector = selector
+    this.height = height + 'px'
+    this.width = width + 'px'
+    this.bg = bg
+    this.fontSize = fontSize + 'px'   
 }
-showDate.start()
+
+
+
+DomElement.prototype.createDiv = function(){
+    newBlock = document.createElement('div')
+    newBlock.style.cssText='height:' + this.height + ';' + 'width:' +  this.width +';' + 'background:' + this.bg + ';' + 'font-size:' + this.fontSize + ';'
+    newBlock.setAttribute('class', this.selector)
+    form.after(newBlock)
+}
+
+
+function shiftDiv (e){
+    let computedStyle = getComputedStyle(divToShift)
+    let left
+    let top
+
+    if (e.keyCode == 37) { // Left
+        left = parseInt(computedStyle.left) - 10
+    }
+    if (e.keyCode == 38) { // Up
+        top = parseInt(computedStyle.top) - 10
+    }
+    if (e.keyCode == 39) { // Right
+        left = parseInt(computedStyle.left) + 10
+    }
+    if (e.keyCode == 40) { // Down
+        top = parseInt(computedStyle.top) + 10
+    }
+    divToShift.style.left = left + 'px';
+    divToShift.style.top = top + 'px';
+}
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    shiftingDiv = new DomElement('shifting-div', '100', '100', 'purple', '25')
+    shiftingDiv.createDiv()
+
+    divToShift = document.querySelector('.shifting-div')
+    divToShift.style.position = 'absolute'
+
+})
+
+document.addEventListener('keydown', shiftDiv)
+
+  
+
+    
+
+
+
+
+
+
+
+
+
